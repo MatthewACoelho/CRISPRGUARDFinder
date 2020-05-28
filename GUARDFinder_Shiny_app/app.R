28e7dfa9f813d4238ae59bfcf12b380ef9a4d0e4
MatthewACoelho <mc32@sanger.ac.uk>
2020-05-28 10:42:29 +0100

    app.R

--- GUARDFinder_Shiny_app/app.R
+++ GUARDFinder_Shiny_app/app.R
@@ -29,16 +29,16 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             textInput("id",
-                        "guide RNA name (e.g. HBB)"),
+                        "guide RNA name (e.g. FANCF)"),
         textInput("guide",
-                  "guide RNA sequence without PAM (e.g. CTTGCCCCACAGGGCAGTAA)"),
+                  "guide RNA sequence without PAM (e.g. GGAATCCCTTCTGCAGCACC)"),
         selectInput("genome",
                   "genome version - human", c("hg38")),
         selectInput("pam",
                   "PAM", c("NGG")),
         sliderInput(inputId = "guide_mismatches",
                   label = "mismatches for the guide RNA off-target search", 
-                  value = 3, min = 0, max = 5),
+                  value = 4, min = 0, max = 5),
         selectInput("guide_min_pvalue",
                   "probability confidence threshold for the guide RNA off-target search", c("0.1", "0.05", "0.01")),
         selectInput("guard_length",
@@ -88,7 +88,7 @@ ui <- fluidPage(
             The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
             THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."),
             hr(),
-            h6("Shiny app development by Matt Coelho, CRISPR GUARD Finder code by Mike Firth. Thanks to Donny van de Meer and Jaymin Mistry for advice.")
+            h6("Shiny app development by Matt Coelho, CRISPR GUARD Finder code by Mike Firth. With thanks to Nick Boughton.")
          )
     )
 )
